bde372c4cdca2fb26a15b965f2f0ad9002408993
juhyun419 <zzang9ha@naver.com>
2024-11-29 21:54:51 +0900

    Initial commit

---
+++ src/main/kotlin/com/jh/blog/ReviewBlogApplication.kt
@@ -0,0 +1,11 @@
+package com.jh.blog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviewBlogApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReviewBlogApplication>(*args)
+}
