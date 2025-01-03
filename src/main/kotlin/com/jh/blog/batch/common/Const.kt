2e519877760eee743f3fc1aeb3a15ea3ee94c32b
juhyun419 <zzang9ha@naver.com>
2025-01-04 08:49:53 +0900

    [#16] feat: add GS exclude keywords

--- src/main/kotlin/com/jh/blog/batch/common/Const.kt
+++ src/main/kotlin/com/jh/blog/batch/common/Const.kt
@@ -6,7 +6,7 @@ object Const {
     const val GS_URL =
         "https://www.cometoplay.kr/item_list.php?category_id=001012024053&sst=it_datetime&sod=desc&page="
     val GS_EXCLUDE_KEYWORDS =
-        listOf("신사", "신논현", "압구정", "신사역", "양재", "양재역", "강남구청", "강남구청역", "수서", "가로수길", "논현역", "학동역")
+        listOf("신사", "신논현", "압구정", "신사역", "양재", "양재역", "강남구청", "강남구청역", "수서", "가로수길", "논현역", "학동역", "청계산", "청계숲")
 
     /* 구로 관악 금천 */
     const val GGG_URL =
